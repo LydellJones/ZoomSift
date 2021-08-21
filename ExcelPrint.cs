5e06483b15524cdc6828cd2be34953e1eae7fec4
Lydell <lydellj1233@gmail.com>
2021-08-20 22:23:43 -0700

    Functional Update 1.2
    Inputs all of the contents of fullset to ExcelPrint.
    New Variables:
    Payload
    load

--- ExcelPrint.cs
+++ ExcelPrint.cs
@@ -1,97 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Text;
+using System;
 using Excel = Microsoft.Office.Interop.Excel;
-using System.IO;
 
 namespace Consoletestwork
 {
     public class ExcelPrint : ExcelParse
     {
-        private string[] colindexary = {"A", "B", "C", "D", "E", "F", "G", "H", "I",
-            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
-            "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI",
-            "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW",
-            "AX", "AY", "AZ"};//list of column identifiers
-        private int rowindex = 1;//regular indexs
-        private int colindex = 0;
-        private int searchrindex = 1;//index for searching out cells
-        private int searchcindex = 0;
-        private bool samefound;//bool for if there is a match
-        private string authorcheck;//checks against the actual author of the message
-        private int rowindexcheck = 1;//indexing for checking rows
-        
-        Excel.Application shopapp = new Excel.Application();
-        
-        public ExcelPrint()
+        private string[,] Payload;
+        private string load;
+        public ExcelPrint(string[,] payload)
         {
-            Excel._Worksheet sheet = (Excel.Worksheet)shopapp.ActiveSheet;
+            Payload = payload;
+        }
+
+        public void Print()
+        {
+            var shopapp = new Excel.Application();
             shopapp.Visible = true;
-        }
-        public void Print()//checcks sfor an existing author first
-        {
-            searchcindex = 0;
-            rowindexcheck = 1;
-
+            shopapp.Workbooks.Add();
             Excel._Worksheet sheet = (Excel.Worksheet)shopapp.ActiveSheet;
-            while (sheet.Cells[1, colindexary[searchcindex]] != null)
-            {
-                authorcheck = (string)sheet.Cells[1, colindexary[searchcindex]];
-                if (authorcheck == author)
-                {
-                    samefound = true;
-                    break;
-                }
-                searchcindex++;
-            }
-            if (samefound == true)//if existing author exists then it should add values under the author's name or under the preexisting values
-            {
-                while (sheet.Cells[rowindexcheck, colindexary[searchcindex]] != null)
-                {
-                    rowindexcheck++;
-                }
-                for (int i = 0; i < printableresults.Length; i++)
-                {
-                    if (printableresults != null)
-                    {
-                        sheet.Cells[rowindexcheck, colindexary[searchcindex]] = printableresults[i];
-                        rowindexcheck++;
-                    }
-                    else
-                    {
-                        rowindexcheck = 0;
-                        break;
-                    }
-                    rowindexcheck = 0;
-                }
 
-            }
-            else //if not then it should create the author on the first row, then under the name, provide values
+            for (int o = 0; o < fullset.GetLength(0); o++)
             {
-                while (sheet.Cells[1, colindexary[colindex]] != null)
+                for (int i = 0; i < fullset.GetLength(1); i++)
                 {
-                    colindex++;
-                }
-                sheet.Cells[1, colindex] = author;
-                for (int i = 0; i < printableresults.Length; i++)
-                {
-                    if (printableresults != null)
-                    {
-                        sheet.Cells[rowindexcheck, colindexary[searchcindex]] = printableresults[i];
-                        rowindexcheck++;
+                    
+                    load = (string)Payload[o, i];
+                    if (load != null) {
+                        sheet.Cells[o + 1, i+1] = load;
                     }
-                    else
-                    {
-                        rowindexcheck = 0;
-                        break;
-                    }
-                    rowindexcheck = 0;
                 }
             }
-            rowindexcheck = 1;
-            searchcindex = 0;
+            Console.WriteLine("Done!");
+        }                    
 
-        }
     }
 }
